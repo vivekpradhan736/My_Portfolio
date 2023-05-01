f053f6874574f3eaec2eed30032562cefa7aa2c8
Vivek <vivekpradhan736@gmail.com>
2023-05-02 02:29:41 +0530

    app js commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-// import components
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
@@ -12,7 +10,6 @@ import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import BackToTopBtn from './components/BackToTopBtn';
-
 
 const App = () => {
   return <div>
