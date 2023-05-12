d5cf8b6e3782e9979b7f0ef8f5898a42fa14eb18
Vivek <vivekpradhan736@gmail.com>
2023-05-12 06:19:39 +0530

    swiper update

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
         <section id='about' className='section bg-secondary '>
             <div className="container  mx-auto">
                 <div className="container flex flex-col xl:flex-row gap-24">
-                    <img className=' object-cover h-full  sm:w-[300px] md:w-[350px] lg:w-[400px] md:mx-auto lg:mx-0 rounded-2xl' src={Image} alt="" />
+                    <img className=' object-cover h-full  sm:w-[300px] md:w-[350px] lg:w-[400px] sm:mx-auto md:mx-auto lg:mx-0 rounded-2xl' src={Image} alt="" />
                     <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
                         <div className="flex flex-col">
                             <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block'>
--- src/components/ExperienceSlider.js
+++ src/components/ExperienceSlider.js
@@ -9,52 +9,45 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 // import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import "swiper/css/navigation";
 import '../swiper.css';
 
 // import required modules
-import { Autoplay, Pagination } from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 
 const ExperienceSlider = () => {
-    // const { authorImg, authorText, authorName, authorPosition } = item;
-
 
     return (
         <>
-            {/* <Swiper pagination={{
-                clickable: true,
-            }}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
                 }}
-                modules={{ Autoplay, Pagination }}
-                className='mySwiper'
-            > */}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
                 <div className='snap-x flex snap-mandatory w-full overflow-x-scroll'>
-                    {experience.map((item, index) => 
-                        (<div className='snap-start w-screen flex flex-col items-center '>
-                            {/* <SwiperSlide key={index}> */}
-                                <div className='flex  w-screen gap-12 lg:gap-32 max-sm:mb-6 '>
-                                    <div className='w-48 h-48 lg:w-[250px] lg:h-[328px]'>
-                                        <img className='rounded-2xl' src={item.experienceImg} alt="" />
+                    {experience.map((item, index) =>
+                        <SwiperSlide key={index}>
+                            <div className='container flex flex-col xl:flex-row gap-8'>
+                                <img className='flex object-cover h-full items-center w-56 sm:w-[230px] md:w-[250px] lg:w-[250px] mx-auto sm:mx-auto md:mx-auto lg:mx-0 rounded-2xl' src={item.experienceImg} alt="" />
+                                <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+                                    <div className="flex flex-col">
+                                        <h2 className='text-3xl lg:text-4xl font-medium lg:font-extrabold relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block '>{item.experienceText}</h2>
+                                        <h4 className='mb-4 text-accent'>{item.experienceTime}</h4>
+                                        <hr className='mb-8 opacity-5' />
+                                        <p className='mb-8 text-orange-50'>{item.experienceTitle}</p>
                                     </div>
-                                    <div className="flex flex-col lg:max-w-3xl max-w-xs">
-                                        <h2 className='font-body mb-2 text-3xl font-bold underline '>{item.experienceText}</h2>
-                                        <h4 className='font-body mb-4 text-1.5xl text-accent '>{item.experienceTime}</h4>
-                                        <div>
-                                            <p className='text-lg text-gray-300'>{item.experienceTitle}</p>
-                                            {/* <p>{item.authorPosition}</p> */}
-                                        </div>
-                                    </div>
-                                    
                                 </div>
-                            {/* </SwiperSlide> */}
-                        </div>)
-
-
+                            </div>
+                        </SwiperSlide>
                     )}
                 </div>
-            {/* </Swiper> */}
+            </Swiper>
         </>
     )
 }
